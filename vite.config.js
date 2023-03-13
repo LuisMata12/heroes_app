c8c955e91b1299341eb7597185af1f62bffeec79
LuisMata <mata.ramirez.1iv14@gmail.com>
2023-03-12 21:26:37 -0600

    build 4

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://LuisMata12.github.io/heroes_app/'
+  base:'https://luisMata12.github.io/heroes_app/'
 })
